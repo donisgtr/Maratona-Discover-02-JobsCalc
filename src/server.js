ef52bef9e7997b1a2e3fbaeb581fb1a0f88b2a29
donissousa <donisgtr@gmail.com>
2021-06-03 19:59:53 -0300

    recomeço

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const server = express()
 
+//habilitar arquivos statics
+server.use(express.static("public")); //middwewe 
+
 server.get('/', (resquet, response) => {
-    return response.send("OIE!!!")
+    return response.sendFile(__dirname + "/views/index.html")
 })
 
 server.listen(3000, () => console.log("rodando"))
