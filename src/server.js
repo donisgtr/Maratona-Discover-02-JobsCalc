8711150b61a989ed3ce7535cc4645f498868d4cc
donissousa <donisgtr@gmail.com>
2021-06-05 16:16:09 -0300

    aula 04

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const server = express()
+const routes = require("./routes")
+
+server.set('view engine', 'ejs')
 
 //habilitar arquivos statics
 server.use(express.static("public")); //middwewe 
 
-server.get('/', (resquet, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
+server.use(routes);
 
 server.listen(3000, () => console.log("rodando"))
